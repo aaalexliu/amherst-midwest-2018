d3caa108bcf8bba91207f96e1af4edadb42472b2
alexsiqingliu <uilxela7@gmail.com>
2018-04-03 15:28:18 -0400

    michael's  momentum strat edited to actually work

--- sample_strategy.py
+++ sample_strategy.py
@@ -21,27 +21,63 @@ class SampleStrategy(PortfolioGenerator):
         pass
 
     def build_signal(self, stock_features):
-        df = stock_features.groupby(
-            'timestep', group_keys=False).apply(self.grp)
-        time = max(df.index)
-        # print(time)
-        # print(df.index)
-        latest = df[df.index == time].copy()
-        # print(latest)
-        latest['hml_signal'] = 0
-        latest.loc[latest.book_market_ranks == 'growth', 'hml_signal'] = -100
-        latest.loc[latest.book_market_ranks == 'value', 'hml_signal'] = 100
+        time = max(stock_features.index)
 
-        latest['smb_signal'] = 0
-        latest.loc[latest.market_cap_ranks == 'small_cap', 'smb_signal'] = -100
-        latest.loc[latest.market_cap_ranks == 'big_cap', 'smb_signal'] = 100
-        latest = latest.assign(signal=
-            (latest.smb_signal + latest.hml_signal)/2, axis=0)
+        umd = self.month_momentum(stock_features)
+        latest = stock_features[stock_features.index == time].copy()
+
+        latest = latest.merge(umd, how = 'left', on = 'ticker')
+        latest['umd_signal'] = 0
+        latest.loc[latest.momentum_rank == 'highest', 'umd_signal'] = 100
+        latest.loc[latest.momentum_rank == 'lowest', 'umd_signal'] = -100
+        # print(latest)
+        # latest = latest.groupby(
+        #     'timestep', group_keys=False).apply(self.grp)
+        # # print(time)
+        # # print(df.index)
+        # # print(latest)
+        # latest['hml_signal'] = 0
+        # latest.loc[latest.book_market_ranks == 'growth', 'hml_signal'] = -100
+        # latest.loc[latest.book_market_ranks == 'value', 'hml_signal'] = 100
+
+        # latest['smb_signal'] = 0
+        # latest.loc[latest.market_cap_ranks == 'small_cap', 'smb_signal'] = -20
+        # latest.loc[latest.market_cap_ranks == 'big_cap', 'smb_signal'] = 20
+        latest = latest.assign(signal = latest.umd_signal)
+        # signal=(latest.smb_signal + latest.hml_signal) / 2)
         # print(latest['signal'])
-        print(latest)
+        # print(latest)
 
         return latest.set_index('ticker')['signal']
         # return self.momentum(stock_features)
+
+    def month_momentum(self, data):
+        result = pd.DataFrame(columns=['ticker', 'momentum'])
+
+        for x in range(0, 1000):
+            ticker_df = data[data.ticker == x]
+        #     ticker_df['momentum'] = ticker_df.pivot_table(index='timestep', values='returns', aggfunc=lambda x: np.cumprod(1 + x.tail(50).head(30)).tail(1))
+        #     print(ticker_df.returns)
+        #     display(ticker_df)
+            cum = ticker_df.returns + 1
+        #     print('just plus one')
+        #     display(cum[1:20])
+            total = np.cumprod(cum[0:30]) * 100
+            # print(len(total))
+            # print(total.iloc[29])
+            # display(total[20:29])
+        #     print(total.colnames())
+        #     print('michael momentum')
+        #     display(ticker_df.momentum[1:20])
+            # print(total.iloc[29])
+            result = result.append({'ticker': x, 'momentum': total.iloc[29]},
+                                   ignore_index=True)
+        result['rank'] = result['momentum'].rank(method='first')
+        dec = pd.qcut(result['rank'], [0, 0.3, 0.7, 1], labels=[
+                      'lowest', 'middle', 'highest'], duplicates='drop')
+        result['momentum_rank'] = dec
+        print(result)
+        return result
 
     def grp(self, data):
         data = data.assign(book_to_market=1 / data.pb)
@@ -49,8 +85,8 @@ class SampleStrategy(PortfolioGenerator):
                       labels=['growth', 'neutral', 'value'])
         data['book_market_ranks'] = dec
 
-        smb = pd.qcut(data['market_cap'], [0, 0.5, 1],
-                      labels=['small_cap', 'big_cap'])
+        smb = pd.qcut(data['market_cap'], [0, 0.1, 0.9, 1],
+                      labels=['small_cap', 'middle', 'big_cap'])
         data['market_cap_ranks'] = smb
         return data
 
